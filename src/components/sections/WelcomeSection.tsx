49057ae8457316d5f9fedeb38921fde073e99b74
Jose samuel pereira <94576233+PuraFome@users.noreply.github.com>
2025-11-25 22:49:08 -0300

    style: finalizei para a apresentação (#21)

--- src/components/sections/WelcomeSection.tsx
+++ src/components/sections/WelcomeSection.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const WelcomeSection: React.FC = React.memo(() => {
     return (
         <section className="welcome-section">
-            <h1 className="welcome-title">Bem Vindo ao Ludico Manager</h1>
+            <h1 className="welcome-title gradient-title">Bem Vindo ao Ludico Manager</h1>
         </section>
     );
 });
